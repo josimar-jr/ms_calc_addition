20bca27870cc5bb2e0686aab043da83f7da8ec4c
josimar-jr <shikamarujunior@gmail.com>
2020-08-10 19:41:10 -0300

    adiciona os testes para / e /healthcheck

--- test/controllers/healthcheck_controller_test.rb
+++ test/controllers/healthcheck_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class HealthcheckControllerTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert true
+  test "/" do
+    get '/', xhr: true
+
+    result = JSON.parse @response.body
+    
+    assert_equal "ok", result['status']
+  end
+
+  test "/healthcheck" do
+    get healthcheck_url, xhr: true
+
+    result = JSON.parse @response.body
+
+    assert_equal "ok", result['status']
   end
 end
